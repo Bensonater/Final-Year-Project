f6d4d6f6f2d77ec57c66b004a71636a5216222a4
Benson Zhou <me@bensonzhou.dev>
2024-06-18 15:43:01 +0100

    final

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Private Fine-Tuning of Large Lanugage Models
+
+This repository contains the code and evaluation for a encrypted fine-tuning framework.
+
+## Repository Structure 
+- data/ : evaluation datasets and test results
+- model/ : GPT-2 with modified FHE-compliant attention head
+- notebooks/ : Evaluation of classifiers, tractability and other evaluations 
+- fhe_gpt.py : A simple script to perform inference on the encrypted model
+
+## Getting Started 
+
+`pip install -r requirements.txt`
--- fhe_gpt.py
+++ fhe_gpt.py
@@ -6,16 +6,12 @@ from logging import getLogger, ERROR
 getLogger("transformers.modeling_utils").setLevel(ERROR)
 
 model = SingleHeadQGPT2Model.from_pretrained("gpt2", n_bits=4, use_cache=False)
-
 tokenizer = GPT2Tokenizer.from_pretrained("openai-community/gpt2")
 
 
 user_input = """
 In a world where technology reigns supreme, humans struggle to find their place. Amidst the rapid advancements and reliance on artificial intelligence, individuals yearn for a deeper connection to nature and their own humanity,
 """
-
-# input_ids = tokenizer(user_input, return_tensors="pt")
-
 
 tokens = tokenizer.encode(user_input)
 
